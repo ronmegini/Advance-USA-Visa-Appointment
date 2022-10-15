0fa337b7dbc2b669cdfa672da1e1091298c38f07
ron34353435 <ron34353435@gmail.com>
2022-10-15 21:50:47 +0300

    initialize project

---
+++ main.py
@@ -0,0 +1,112 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import datetime
+import time
+import re
+
+class VisaBot():
+    """
+    Object handle the webdriver session which perform the automated actions
+
+    :param email: (str) email used for login
+    :param password: (str) password used for login
+    """
+    def __init__(self, email, password):
+        # attributes
+        self.driver = webdriver.Chrome() # Locate the right version of chromedriver in the same directory
+        self.email = email
+        self.password = password
+
+        # functions
+        self.login()
+        #WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".application.attend_appointment.card.success")))
+        self.list_customers()
+        time.sleep(10)
+        self.driver.close()
+
+
+    def login(self):
+        """
+        Login into use visa site with the object's credentials.
+        The result is driver's signed in session.
+
+        :returns: None
+        """
+        self.driver.get("https://ais.usvisa-info.com/he-il/niv/users/sign_in")
+        email_field = self.driver.find_element(By.ID, "user_email")
+        email_field.send_keys(self.email)
+        password_field = self.driver.find_element(By.ID, "user_password")
+        password_field.send_keys(self.password)
+        policy_field = self.driver.find_element(By.ID, "policy_confirmed")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(policy_field).perform()
+        self.driver.execute_script("arguments[0].click();", policy_field)
+        submit = self.driver.find_element(By.NAME, "commit").click()
+
+
+    def parse_date(self, raw_string):
+        """
+        Extract date and location from string
+
+        :date: (str) raw location and date string
+
+        :returns:
+        :date: (datetime) date of the original appointment
+        :location: (str) Tel Aviv or Jerusalem
+        """
+        
+        MONTHS = {
+            'ינואר': 'Jan',
+            'פברואר': 'Feb',
+            'מרץ': 'Mar',
+            'אפריל': 'Apr',
+            'מאי': 'May',
+            'יוני': 'Jun',
+            'יולי': 'Jul',
+            'אוגוסט': 'Aug',
+            'ספטמבר': 'Sep',
+            'אוקטובר': 'Oct',
+            'נובמבר': 'Nov',
+            'דצמבר': 'Dec',
+        }
+
+        
+        for key, value in MONTHS.items():
+            raw_string = raw_string.replace(key, value)
+        raw_string = re.sub("[^a-zA-Z0-9]", "",raw_string)
+        if "TelAviv" in raw_string: location="Tel Aviv"
+        if "Jerusalem" in raw_string: location="Jerusalem"
+        raw_string = re.sub("TelAvivatTelAviv", "00",raw_string)
+        raw_string = re.sub("JerusalematJerusalem", "00",raw_string)
+        date = datetime.strptime(raw_string, '%d%b%Y%H%M%S')
+        
+        return date,location
+
+
+
+    def list_customers(self):
+        """
+        Get all customers details under the selected account.
+
+        :returns:
+        :urls: (list of atr) urls of reschedule appointment
+        :current_date: (datetime) date of the original appointment
+        :location: (str) Tel Aviv or Jerusalem
+        """
+
+
+        customers = self.driver.find_elements(By.CSS_SELECTOR, ".application.attend_appointment.card.success")
+        for customer in customers:
+            url = customer.find_element(By.CSS_SELECTOR, ".button.primary.small").get_attribute("href")
+            current_appointment = customer.find_element(By.CLASS_NAME, "consular-appt").text
+            #name = customer.find_element(By.CSS_SELECTOR, "table.medium-12.columns.margin-bottom-20")
+            current_date,location = self.parse_date(current_appointment)
+
+            print("Name: {}, Date: {}, Location: {}, URL: {}".format(name,current_date,location,url))
+        
+
+if __name__ == '__main__':
+    robot = VisaBot("afikbh229@gmail.com","Afikbh229")
